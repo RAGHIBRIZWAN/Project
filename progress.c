d28735b44bdc7a144199aace5af97ccfc698157b
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2023-11-27 19:17:39 +0500

    Create progress.c

---
+++ progress.c
@@ -0,0 +1,252 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include <string.h>
+#include <time.h>
+#include <windows.h>
+#include<unistd.h>
+
+void one(char ch[50], FILE *easy){
+
+    FILE *hs;
+    int sh[5];
+    int highscore = 0;
+    // hs = fopen("highscore.txt" , "r");
+    
+    // int a = fscanf(hs,"%d",highscore);
+    // printf("Highscore is:  %c\n",a);
+
+    // fclose(hs);
+    int streak = 0;
+    time_t starttime, endtime;
+    int elapsedtime;
+    char *arr[100];
+    char answer;
+    int ans1;
+    int score = 0;
+    char brr[10] = {'A','B','D','A','A','B','A','B','B','A'};
+
+    if (easy == NULL)
+    {
+        printf("Unable to open the file");
+    }
+    else{
+        for (int i = 0; i < 10; i++)
+        {   
+            arr[i] = fgets(ch,100,easy);
+            printf(" %s\n",arr[i]);
+            time(&starttime);
+            printf("Enter the answer: ");
+            scanf(" %c",&answer);
+            getchar();
+            time(&endtime);
+            elapsedtime = difftime(endtime,starttime);   
+            
+            printf("Time taken: %d seconds\n", elapsedtime);
+            
+            if(elapsedtime > 10 && answer == brr[i]){
+                printf("Time's up.... 100 points deducted.\n");
+                score = score - 100;
+                streak = 0;
+            }
+            else if(answer == brr[i]){
+                printf("Your answer is Correct!\n");
+                score = score + 100;
+                streak++;
+            }
+            else if(answer != brr[i]){
+                printf("Your answer is Wrong\n");
+                streak = 0;
+            }
+            
+        }
+    }
+    int a = score;
+    printf("Score Is: %d\n",score);
+    printf("Winning streak is: %d",streak);
+    fclose(easy);
+    hs = fopen("highscore.txt" , "w");
+
+    if(highscore < score){ 
+        highscore = score;
+        }
+    fprintf(hs,"%d",highscore);
+    
+    fclose(hs);
+}
+
+void two(char ch[50], FILE *moderate){
+
+    int streak = 0;
+    time_t starttime, endtime;
+    int elapsedtime;
+    char *arr[100];
+    char answer;
+    int ans1;
+    int highscore = 0;
+    char brr[10] = {'A','B','D','A','A','B','A','B','B','A'};
+
+    if (moderate == NULL)
+    {
+        printf("Unable to open the file");
+    }
+    else{
+        for (int i = 0; i < 10; i++)
+        {   
+            arr[i] = fgets(ch,100,moderate);
+            printf(" %s\n",arr[i]);
+            time(&starttime);
+            printf("Enter the answer: ");
+            scanf(" %c",&answer);
+            getchar();
+            time(&endtime);
+            elapsedtime = difftime(endtime,starttime);   
+            
+            printf("Time taken: %d seconds\n", elapsedtime);
+            
+            if(elapsedtime > 20 && answer == brr[i]){
+                printf("Time's up.... 100 points deducted.\n");
+                highscore = highscore - 100;
+                streak = 0;
+            }
+            else if(answer == brr[i]){
+                printf("Your answer is Correct!\n");
+                highscore = highscore + 100;
+                streak++;
+            }
+            else if(answer != brr[i]){
+                printf("Your answer is Wrong\n");
+                streak = 0;
+            }
+            
+        }
+    }
+    printf("Score Is: %d\n",highscore);
+    printf("Winning streak is: %d",streak);
+    fclose(moderate);
+    moderate = fopen("simple.txt" , "a");
+    fprintf(moderate,"\nYour high score is: %d",highscore);
+    fclose(moderate);
+}
+
+void three(char ch[50], FILE *hard){
+
+    int streak = 0;
+    time_t starttime, endtime;
+    int elapsedtime;
+    char *arr[100];
+    char answer;
+    int ans1;
+    int highscore = 0;
+    char brr[10] = {'A','B','D','A','A','B','A','B','B','A'};
+
+    if (hard == NULL)
+    {
+        printf("Unable to open the file");
+    }
+    else{
+        for (int i = 0; i < 10; i++)
+        {   
+            arr[i] = fgets(ch,100,hard);
+            printf(" %s\n",arr[i]);
+            time(&starttime);
+            printf("Enter the answer: ");
+            scanf(" %c",&answer);
+            getchar();
+            time(&endtime);
+            elapsedtime = difftime(endtime,starttime);   
+            
+            printf("Time taken: %d seconds\n", elapsedtime);
+            
+            if(elapsedtime > 30 && answer == brr[i]){
+                printf("Time's up.... 100 points deducted.\n");
+                highscore = highscore - 100;
+                streak = 0;
+            }
+            else if(answer == brr[i]){
+                printf("Your answer is Correct!\n");
+                highscore = highscore + 100;
+                streak++;
+            }
+            else if(answer != brr[i]){
+                printf("Your answer is Wrong\n");
+                streak = 0;
+            }
+            
+        }
+    }
+    printf("Score Is: %d\n",highscore);
+    printf("Winning streak is: %d",streak);
+    fclose(hard);
+    hard = fopen("simple.txt" , "a");
+    fprintf(hard,"\nYour high score is: %d",highscore);
+    fclose(hard);
+}
+
+int main(){
+    char name[50];
+    char ans;
+    int diff;
+    char answer;
+    char ch[10];
+    FILE *easy = fopen("simple.txt" , "r");
+    FILE *moderate = fopen("moderate.txt", "r");
+    FILE *hard = fopen("hard.txt", "r");
+
+    printf("Enter yourn name:");
+    gets(name);
+    printf("\n");
+    printf("%s WELCOME TO MATHAMANIA\n\n",name);
+
+    printf("Do You want to start game? 'y' for yes and 'n' for no: ");
+    scanf("%c",&ans);
+    switch (ans)
+    {
+    case 'y':
+        system("cls");
+        printf("Lets start the game!\n\n");
+        break;
+
+    case 'Y':
+        system("cls");
+        printf("Lets start the game!\n\n");
+        break;
+
+    case 'n':
+        exit(1);
+        break;
+    
+    case 'N':
+        exit(1);
+        break;
+
+    default:
+        exit(1);
+        break;
+    }
+
+    printf("Select Difficulty: \n");
+    printf("1. Easy    2. Moderate    3. Hard:  ");
+    scanf("%d",&diff);
+
+    switch (diff)
+    {
+    case 1:
+
+    one(ch,easy);
+    break;
+
+    case 2:
+
+    two(ch,moderate);
+    break;
+
+    case 3:
+
+    three(ch,hard);
+    break;
+    
+    default:
+        break;
+    }
+}
